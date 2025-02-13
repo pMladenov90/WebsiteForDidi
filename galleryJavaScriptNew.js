e038bc8cc94f2a1b17543013c2f8ae47e0da787f
Peter Mladenov <p.mladenov90@gmail.com>
2025-02-13 18:31:50 +0200

    Trying to integrare subsections in gallery page

---
+++ galleryJavaScriptNew.js
@@ -0,0 +1,15 @@
+// JavaScript for smooth scrolling (if needed)
+document.querySelectorAll('.dropdown-content a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
